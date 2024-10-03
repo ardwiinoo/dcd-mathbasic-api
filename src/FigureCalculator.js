5a22a71fc19a90b87b05c6dfee31e2efc3b290fe
Arif Dwi Nugroho <ardwiinoo@gmail.com>
2024-10-03 20:45:40 +0700

    add checking type args

--- src/FigureCalculator.js
+++ src/FigureCalculator.js
@@ -7,6 +7,13 @@ class FigureCalculator {
         if (args.length !== 2) {
             throw new Error('fungsi hanya menerima dua parameter')
         }
+
+        const [length, width] = args
+ 
+ 
+        if (typeof length !== 'number' || typeof width !== 'number') {
+            throw new Error('fungsi hanya menerima parameter number')
+        }
     }
 
     calculateRectangleArea() { }

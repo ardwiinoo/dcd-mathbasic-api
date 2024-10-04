d50ed1e7e65d853176d028cbaa96a6f6bb30b9b4
Arif Dwi Nugroho <ardwiinoo@gmail.com>
2024-10-05 01:52:22 +0700

    add subtract endpoint

--- src/createServer.js
+++ src/createServer.js
@@ -1,5 +1,5 @@
 const Hapi = require('@hapi/hapi')
-
+ 
 const createServer = ({ mathBasic }) => {
     const server = Hapi.server({
         host: 'localhost',
@@ -11,10 +11,19 @@ const createServer = ({ mathBasic }) => {
         method: 'GET',
         path: '/add/{a}/{b}',
         handler: (request) => {
-            const { a, b } = request.params
-            const value = mathBasic.add(Number(a), Number(b))
-            return { value }
+                const { a, b } = request.params
+                const value = mathBasic.add(Number(a), Number(b))
+                return { value }
+            },
         },
+        {
+        method: 'GET',
+        path: '/subtract/{a}/{b}',
+        handler: (request) => {
+                const { a, b } = request.params
+                const value = mathBasic.subtract(Number(a), Number(b))
+                return { value }
+            },
         },
     ])
     
